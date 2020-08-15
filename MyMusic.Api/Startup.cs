e7e0787e6fd7f450806125c2fc51775c9b3bb31f
adenle mubaraq <adenlemubaraq@gmail.com>
2020-08-15 07:33:21 +0100

    Added  UnitWork dependency to Api project

--- MyMusic.Api/Startup.cs
+++ MyMusic.Api/Startup.cs
@@ -10,6 +10,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using MyMusic.Core;
+using MyMusic.Data;
 
 namespace MyMusic.Api
 {
@@ -25,6 +27,8 @@ namespace MyMusic.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped<IUnitOfWork , UnitOfWork>();
+
             services.AddControllers();
         }
 
